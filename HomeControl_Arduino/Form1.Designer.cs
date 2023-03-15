151470521e5aa562001dc3fe867591fb9356cdd1
HUIKIM0 <fehs123@naver.com>
2023-03-15 19:03:35 +0900

    update_0315

--- HomeControl_Arduino/Form1.Designer.cs
+++ HomeControl_Arduino/Form1.Designer.cs
@@ -29,94 +29,34 @@ namespace HomeControl_Arduino
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.cboxSelectPort = new System.Windows.Forms.ComboBox();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.btnDisconnect = new System.Windows.Forms.Button();
             this.label_status = new System.Windows.Forms.Label();
             this.richTextBox_received = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "COM 포트설정";
-            // 
-            // cboxSelectPort
-            // 
-            this.cboxSelectPort.FormattingEnabled = true;
-            this.cboxSelectPort.Items.AddRange(new object[] {
-            "포트를 선택"});
-            this.cboxSelectPort.Location = new System.Drawing.Point(15, 51);
-            this.cboxSelectPort.Name = "cboxSelectPort";
-            this.cboxSelectPort.Size = new System.Drawing.Size(103, 23);
-            this.cboxSelectPort.TabIndex = 1;
-            this.cboxSelectPort.Click += new System.EventHandler(this.cboxSelectPort_Click);
-            // 
-            // btnConnect
-            // 
-            this.btnConnect.Location = new System.Drawing.Point(125, 46);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(100, 35);
-            this.btnConnect.TabIndex = 2;
-            this.btnConnect.Text = "연결하기";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-            // 
-            // btnDisconnect
-            // 
-            this.btnDisconnect.Location = new System.Drawing.Point(124, 97);
-            this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(101, 32);
-            this.btnDisconnect.TabIndex = 8;
-            this.btnDisconnect.Text = "연결끄기";
-            this.btnDisconnect.UseVisualStyleBackColor = true;
-            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
             // 
             // label_status
             // 
             this.label_status.AutoSize = true;
-            this.label_status.Location = new System.Drawing.Point(15, 159);
+            this.label_status.Location = new System.Drawing.Point(12, 26);
             this.label_status.Name = "label_status";
-            this.label_status.Size = new System.Drawing.Size(45, 15);
+            this.label_status.Size = new System.Drawing.Size(107, 15);
             this.label_status.TabIndex = 9;
-            this.label_status.Text = "label2";
+            this.label_status.Text = "포트 상태 확인";
             // 
             // richTextBox_received
             // 
-            this.richTextBox_received.Location = new System.Drawing.Point(342, 46);
+            this.richTextBox_received.Location = new System.Drawing.Point(137, 23);
             this.richTextBox_received.Name = "richTextBox_received";
             this.richTextBox_received.Size = new System.Drawing.Size(190, 138);
             this.richTextBox_received.TabIndex = 10;
             this.richTextBox_received.Text = "";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(124, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 41);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "form연결테스트용";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 503);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(386, 233);
             this.Controls.Add(this.richTextBox_received);
             this.Controls.Add(this.label_status);
-            this.Controls.Add(this.btnDisconnect);
-            this.Controls.Add(this.btnConnect);
-            this.Controls.Add(this.cboxSelectPort);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -126,14 +66,8 @@ namespace HomeControl_Arduino
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cboxSelectPort;
-        private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.Button btnDisconnect;
         private System.Windows.Forms.Label label_status;
         private System.Windows.Forms.RichTextBox richTextBox_received;
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- HomeControl_Arduino/Form1.cs
+++ HomeControl_Arduino/Form1.cs
@@ -15,11 +15,8 @@ namespace HomeControl_Arduino
     {
         SerialPort mySerial = new SerialPort();
 
+        // 뭐 테스트한다고 퍼블릭으로 해놨는디 나중에 원래대로 하든가 말든가 해야함
         string recievedtext;
-
-        public delegate void delSerialText(string strdelSerialText);
-        public event delSerialText eventdelSerialText;
-
 
 
         public Form1()
@@ -30,56 +27,65 @@ namespace HomeControl_Arduino
         private void Form1_Load(object sender, EventArgs e)
         {
             mySerial = new SerialPort();
+
+
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                try
+                {
+                    if (!mySerial.IsOpen)
+                    {
+
+
+                        mySerial.PortName = port;  //콤보박스의 선택된 COM포트명을 시리얼포트명으로 지정
+                        mySerial.BaudRate = 9600;  //보레이트 변경이 필요하면 숫자 변경하기
+                        mySerial.DataBits = 8;
+                        mySerial.StopBits = StopBits.One;
+                        mySerial.Parity = Parity.None;
+
+                        mySerial.DataReceived += new SerialDataReceivedEventHandler(MySerial_DataReceived);
+
+                        mySerial.Open();
+
+                        int RecvSize = mySerial.BytesToRead;
+                        if (RecvSize == 0)
+                        {
+                            mySerial.Close();
+                        }
+                        else
+                        {
+                            label_status.Text = "포트가 열렸습니다";
+                        }
+                    }
+                    else
+                    {
+                        label_status.Text = "포트가 이미 열려있습니다";
+                    }
+                }
+
+
+                catch (Exception)  //에스파는 둘이 될 수 없는데 Com8이 맞는데.. Com4도 연결되긴했다고 뜸 이거 해결하삼 주희
+                {
+                    continue;
+                }
+
+            }
+
+
+            /*
+            수정전 원본!!!! 깃에 올린 원본!!
+
             cboxSelectPort.DataSource = SerialPort.GetPortNames();
             cboxSelectPort.Text = "포트선택";
-        }
-
-        private void cboxSelectPort_Click(object sender, EventArgs e)
-        {
-           // cboxSelectPort.Items.Clear();
-           // foreach (var item in SerialPort.GetPortNames())
-           // {
-            //    cboxSelectPort.Items.Add(item);
-           // }
-
+            */
 
         }
 
 
-        //통신 연결하기 버튼 Button_connect_Click 선생님버전
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (!mySerial.IsOpen)
-                {
-                    mySerial.PortName = cboxSelectPort.Text;  //콤보박스의 선택된 COM포트명을 시리얼포트명으로 지정
-                    mySerial.BaudRate = 9600;  //보레이트 변경이 필요하면 숫자 변경하기
-                    mySerial.DataBits = 8;
-                    mySerial.StopBits = StopBits.One;
-                    mySerial.Parity = Parity.None;
 
-                    mySerial.DataReceived += new SerialDataReceivedEventHandler(MySerial_DataReceived);
-
-                    mySerial.Open();
-
-                    label_status.Text = "포트가 열렸습니다";
-                    cboxSelectPort.Enabled = false;
-                }
-
-                else
-                {
-                    label_status.Text = "포트가 이미 열려있습니다";
-                }
-            }
-            catch (Exception)  //에스파는 둘이 될 수 없는데 Com8이 맞는데.. Com4도 연결되긴했다고 뜸 이거 해결하삼 주희
-            {
-                MessageBox.Show("연결에러", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-           
-        }
-
+        #region 사용자 정의함수
         private void MySerial_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             this.Invoke(new EventHandler(MySerialReceived));
@@ -87,55 +93,12 @@ namespace HomeControl_Arduino
 
 
         private void MySerialReceived(object s, EventArgs e)  //여기에서 수신 데이타를 사용자의 용도에 따라 처리한다.
-        { 
-
+        {
             recievedtext = mySerial.ReadExisting();
-            richTextBox_received.Text = richTextBox_received.Text + string.Format(recievedtext);
 
+            richTextBox_received.Text = recievedtext;
+            //richTextBox_received.Text = richTextBox_received.Text + string.Format(recievedtext);
         }
-
-
-        private void btnDisconnect_Click(object sender, EventArgs e)
-        {
-            if (mySerial.IsOpen)  //시리얼포트가 열려 있으면
-            {
-                mySerial.Close();  //시리얼포트 닫기
-
-                label_status.Text = "포트가 닫혔습니다.";
-                cboxSelectPort.Enabled = true;  //COM포트설정 콤보박스 활성화
-            }
-            else  //시리얼포트가 닫혀 있으면
-            {
-                label_status.Text = "포트가 이미 닫혀 있습니다.";
-            }
-        }
-
-
-
-        //폼 연결 테스트용 삭제예정 ★
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-
-            FormHome childForm = new FormHome();
-            childForm.ShowDialog();
-
-        }
-
-        /* 삭제용.. 시리얼통신한 Text결과를 List에 넣고 뿌려주고 싶었는데.. 
-         * 시리얼 통신은 A출력 (끝) B출력(끝) 이런느낌으로 1초마다 돌아가는지라 몰루?*/
-        public static List<string> SendChildFormMySerialText(string str)
-        {
-
-            List<string> Liststr = new List<string>();
-            foreach (var item in Liststr)
-            {
-                Liststr.Add(item);
-            }
-
-            return Liststr;
-
-        }
-
+        #endregion
     }
 }
